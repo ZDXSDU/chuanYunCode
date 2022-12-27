93db038a3ae0f5eff050503dae682bb23a16a33f
Z.D.X SDU <2398463125@qq.com>
2022-12-27 13:47:37 +0800

    自定义打印页面

--- 氚云自定义打印模板 - 友好售后配件标签/H3列表前端.js
+++ 氚云自定义打印模板 - 友好售后配件标签/H3列表前端.js
@@ -1,6 +1,60 @@
+/*
+* $.ListView.GetSelected()获取选中的记录
+* $.ListView.RefreshView()刷新列表
+* $.ListView.Post()请求后台
+* $.ListView.InitQueryItems()修改过滤条件
+* $.ListView.RefreshView()刷新页面
+* $.ListView.ActionPreDo() 按钮执行之前的事件
+*/
+
 $.ListView.ActionPreDo = function( actionCode ) {
     if( actionCode == 'smallLabel' ) { // 打印小标签
+        if( actionCode == 'smallLabel' ) {
+            let objects = $.ListView.GetSelected();
+            let OBJs = '';
+            if( objects == null || objects == undefined || objects == "" ) {
+                $.IShowWarn( '你没有选中任何数据,请勾选！' );
+                return;
+            }
+            else {
+                for( let i = 0;i < objects.length;i++ ) {
+                    OBJs += ( objects[ i ].ObjectId + ',' );
+                }
+                setTimeout(() => {
+                    $.ListView.Post( "smallLabel", {
+                        OBJs: OBJs.substr( 0, OBJs.length - 1 )
+                    }, ( data ) => {
+                        if( data.ReturnData.res.length > 0 ) {
+                            let alize = `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Document</title><style>table{border-collapse: collapse;}div td {border-style: dashed;}</style></head><body><div><table><tr><td >配件号：</td><td >出库价：</td></tr><tr><td>配件名称：</td><td >适用车型：</td></tr><tr><td>位置码：</td><td >高限</td><td >低限</td></tr></table></div></body></html>`;
+                            for( let i = 0;i < data.ReturnData.res.length;i++ ) {
+                                // alize += `<table cellspacing="0" cellpadding="0"><tr><td class="bTop bRight bLeft" style="text-align: center;font-weight: 600;" colspan="4">` + data.ReturnData.res[ i ].BU + "事业部" + data.ReturnData.res[ i ].BR + "店</td></tr>"; // TH
+                                alize += `<tr><td colspan="2" class="bTop bRight bBottom bLeft TDl">配件号</td><td colspan="2" class="bTop bRight bBottom">` + data.ReturnData.res[ i ].PartNo + "</td></tr>"; // 配件号
+                                alize += `<tr><td colspan="2" class="bRight bBottom bLeft TDl">配件名称</td><td colspan="2" class="bRight bBottom">` + data.ReturnData.res[ i ].PartName + "</td></tr>"; // 配件名称
+                                alize += `<tr><td colspan="2" class="bRight bBottom bLeft TDl">货位号</td><td colspan="2" class="bRight bBottom">` + data.ReturnData.res[ i ].Cargospace + "</td></tr>"; // 货位号
+                                alize += `<tr><td colspan="2" class="bRight bBottom bLeft TDl">适用车型</td><td colspan="2" class="bRight bBottom">` + data.ReturnData.res[ i ].Model + "</td></tr>"; // 适用车型
+                                alize += `<tr><td class="bRight bBottom bLeft TDll">出库价</td><td class="bRight bBottom">` + data.ReturnData.res[ i ].OutPrice + `</td><td class="bRight bBottom TDll">周转率</td><td class="bRight bBottom">--%</td></tr>`; // 出库价 周转率
+                                // alize += `<tr><td class="bRight bBottom bLeft TDll">备注</td><td colspan="3" class="bRight bBottom">没有备注</td></tr></table>`; // 备注
+                                if( ( i + 1 ) % 2 === 0 && ( i + 1 ) > 1 ) { alize += `<div class="line"></div>`; }
+                                // if( ( i + 1 ) % 5 === 0 && ( i + 1 ) > 4 ) { alize += `<div class="pageTo"></div>`; }
 
+                            }
+                            alize += "</div></div>";
+                            console.log( alize );
+                            win = window.open();
+                            win.document.write( alize );
+                        } else {
+                            if( data.ReturnData != null && data.ReturnData.Message ) {
+                                $.IShowError( data.ReturnData.Message );
+                            } else {
+                                $.IShowError( '操作失败！请联系管理员处理' );
+                            }
+                        }
+                    }, ( data ) => {
+                        $.IShowError( '操作失败！请联系管理员处理' );
+                    }, false );
+                }, 1000 )
+            }
+        }
     }
     if( actionCode == 'BigLabel' ) {
         let objects = $.ListView.GetSelected();
@@ -18,38 +72,22 @@ $.ListView.ActionPreDo = function( actionCode ) {
                     OBJs: OBJs.substr( 0, OBJs.length - 1 )
                 }, ( data ) => {
                     if( data.ReturnData.res.length > 0 ) {
-                        let alize = `<div class='a'><style>html {font-family: sans-serif;line-height: 1.15;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;} body {margin: 0;}article,aside,
-                        footer,header,nav,section {display: block;}h1 {font-size: 2em;margin: 0.67em 0;}figcaption,figure,main {display: block;} figure {margin: 1em 40px;}hr {box-sizing: content-box;height: 0;
-                        overflow: visible;}pre {font-family: monospace, monospace;font-size: 1em;} a {background-color: transparent;-webkit-text-decoration-skip: objects;}a:active,a:hover {outline-width: 0;} 
-                        abbr[title] {border-bottom: none; text-decoration: underline;text-decoration: underline dotted;}b,strong {font-weight: inherit;}b,strong {font-weight: bolder;}code,kbd,samp {font-family: 
-                        monospace, monospace;font-size: 1em;}dfn {font-style: italic;}mark {background-color: #ff0;color: #000;}small {font-size: 80%;}sub,sup {font-size: 75%;line-height: 0;position: relative;
-                        vertical-align: baseline;}sub {bottom: -0.25em;}sup {top: -0.5em;}audio,video {display: inline-block;}audio:not([controls]) {display: none;height: 0;}img {border-style: none;}svg:not(:root) {
-                        overflow: hidden;}button,input,optgroup,select,textarea {font-family: sans-serif;font-size: 100%;line-height: 1.15;margin: 0;}button,input {overflow: visible;}button,select {
-                        text-transform: none;}button,html [type="button"],[type="reset"],[type="submit"] {-webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner,
-                        [type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {border-style: none;padding: 0;}button:-moz-focusring,[type="button"]:-moz-focusring,[type="reset"]:-moz-focusring,
-                        [type="submit"]:-moz-focusring {outline: 1px dotted ButtonText;}fieldset {border: 1px solid #c0c0c0;margin: 0 2px;padding: 0.35em 0.625em 0.75em;}legend {box-sizing: border-box;
-                        color: inherit;display: table;max-width: 100%;padding: 0;white-space: normal;}progress {display: inline-block;vertical-align: baseline;}textarea {overflow: auto;}[type="checkbox"],
-                        [type="radio"] {box-sizing: border-box;padding: 0;}[type="number"]::-webkit-inner-spin-button,[type="number"]::-webkit-outer-spin-button {height: auto;}[type="search"] 
-                        {-webkit-appearance: textfield;outline-offset: -2px;}[type="search"]::-webkit-search-cancel-button,[type="search"]::-webkit-search-decoration {-webkit-appearance: none;}
-                        ::-webkit-file-upload-button {-webkit-appearance: button;font: inherit;}details,menu {display: block;}summary {display: list-item;}canvas {display: inline-block;}template {display: none;}
-                        [hidden] {display: none;}</style><style>.a {width: 210mm;height: 340mm;font-size: 14pt;font-family: SimSun, 宋体, serif;color: black;line-height: 1.4;text-align: justify;margin: 0;padding: 0;}
-                        .b {width: 210mm;height: 340mm;padding: 0 24mm 0 24mm;background-color: #fff;}table {width: 45%;float: left;margin: 3.0% 2.5%;}td {margin: 0;padding-left: 6px;}.TDl {width: 45%;}.TDll {width: 20%;
-                        }.bTop {border-top: 1px solid #555;}.bRight {border-right: 1px solid #555;}.bBottom {border-bottom: 1px solid #555;}.bLeft {border-left: 1px solid #555;}.line {width: 100%;
-                        border-bottom: 1px dashed #555;*zoom: 1;}.line::after {visibility: hidden;clear: both;display: block;content: ".";height: 0}</style><div class='b'>`;
+                        let alize = `<div class='a'><style>html {font-family: sans-serif;line-height: 1.15;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%;} body {margin: 0;}article,aside, footer,header,nav,section {display: block;}h1 {font-size: 2em;margin: 0.67em 0;}figcaption,figure,main {display: block;} figure {margin: 1em 40px;}hr {box-sizing: content-box;height: 0; overflow: visible;}pre {font-family: monospace, monospace;font-size: 1em;} a {background-color: transparent;-webkit-text-decoration-skip: objects;}a:active,a:hover {outline-width: 0;}                       abbr[title] {border-bottom: none; text-decoration: underline;text-decoration: underline dotted;}b,strong {font-weight: inherit;}b,strong {font-weight: bolder;}code,kbd,samp {font-family: monospace, monospace;font-size: 1em;}dfn {font-style: italic;}mark {background-color: #ff0;color: #000;}small {font-size: 80%;}sub,sup {font-size: 75%;line-height: 0;position: relative; vertical-align: baseline;}sub {bottom: -0.25em;}sup {top: -0.5em;}audio,video {display: inline-block;}audio:not([controls]) {display: none;height: 0;}img {border-style: none;}svg:not(:root) { overflow: hidden;}button,input,optgroup,select,textarea {font-family: sans-serif;font-size: 100%;line-height: 1.15;margin: 0;}button,input {overflow: visible;}button,select { text-transform: none;}button,html [type="button"],[type="reset"],[type="submit"] {-webkit-appearance: button;}button::-moz-focus-inner,[type="button"]::-moz-focus-inner, [type="reset"]::-moz-focus-inner,[type="submit"]::-moz-focus-inner {border-style: none;padding: 0;}button:-moz-focusring,[type="button"]:-moz-focusring,[type="reset"]:-moz-focusring, [type="submit"]:-moz-focusring {outline: 1px dotted ButtonText;}fieldset {border: 1px solid #c0c0c0;margin: 0 2px;padding: 0.35em 0.625em 0.75em;}legend {box-sizing: border-box; color: inherit;display: table;max-width: 100%;padding: 0;white-space: normal;}progress {display: inline-block;vertical-align: baseline;}textarea {overflow: auto;}[type="checkbox"],[type="radio"] {box-sizing: border-box;padding: 0;}[type="number"]::-webkit-inner-spin-button,[type="number"]::-webkit-outer-spin-button {height: auto;}[type="search"] {-webkit-appearance: textfield;outline-offset: -2px;}[type="search"]::-webkit-search-cancel-button,[type="search"]::-webkit-search-decoration {-webkit-appearance: none;} ::-webkit-file-upload-button {-webkit-appearance: button;font: inherit;}details,menu {display: block;}summary {display: list-item;}canvas {display: inline-block;}template {display: none;} [hidden] {display: none;}</style><style>@media print {@page {margin: 1cm 0;}body {margin: 1cm 0;}} .a {width: 210mm;font-size: 14pt;font-family: SimSun, 宋体, serif;color: black;line-height: 1.4;text-align: justify;margin: 0;padding: 0;overflow-y: scroll;} .b {width: 210mm;background-color: #fff;overflow-y: scroll;}table {width: 45%;float: left;margin: 3.0% 2.5%;}td {margin: 0;padding-left: 6px;}.TDl {width: 45%;}.TDll {width: 20%; }.bTop {border-top: 1px solid #555;}.bRight {border-right: 1px solid #555;}.bBottom {border-bottom: 1px solid #555;}.bLeft {border-left: 1px solid #555;}.line {width: 100%; border-bottom: 1px dashed #555;*zoom: 1;}.line::after {visibility: hidden;clear: both;display: block;content: ".";height: 0} .pageTo {page-break-after: always;}.print {position: fixed;top: 60px;left: 250mm;width: 160px;height: 52px;font-size: 26px;color: #000;font-weight: 600;line-height: 52px;text-align: center;font-family: SimSun, 宋体, serif;border-radius: 5px;box-shadow: 2px 2px 4px 4px rgb(0 0 0 / 20%);animation: shine .5s infinite;cursor: pointer;}.print:hover {background-color: #000;color: #fff;}@keyframes shine {0%,100% {box-shadow: 2px 2px 6px 6px rgba(0, 255, 0, 1);}50% {box-shadow: 2px 2px 6px 6px rgba(255, 0, 0, 1);}}</style><div class="print">点我打印</div><div class='b'>`;
                         for( let i = 0;i < data.ReturnData.res.length;i++ ) {
                             alize += `<table cellspacing="0" cellpadding="0"><tr><td class="bTop bRight bLeft" style="text-align: center;font-weight: 600;" colspan="4">` + data.ReturnData.res[ i ].BU + "事业部" + data.ReturnData.res[ i ].BR + "店</td></tr>"; // TH
                             alize += `<tr><td colspan="2" class="bTop bRight bBottom bLeft TDl">配件号</td><td colspan="2" class="bTop bRight bBottom">` + data.ReturnData.res[ i ].PartNo + "</td></tr>"; // 配件号
                             alize += `<tr><td colspan="2" class="bRight bBottom bLeft TDl">配件名称</td><td colspan="2" class="bRight bBottom">` + data.ReturnData.res[ i ].PartName + "</td></tr>"; // 配件名称
                             alize += `<tr><td colspan="2" class="bRight bBottom bLeft TDl">货位号</td><td colspan="2" class="bRight bBottom">` + data.ReturnData.res[ i ].Cargospace + "</td></tr>"; // 货位号
                             alize += `<tr><td colspan="2" class="bRight bBottom bLeft TDl">适用车型</td><td colspan="2" class="bRight bBottom">` + data.ReturnData.res[ i ].Model + "</td></tr>"; // 适用车型
-                            alize += `<tr><td class="bRight bBottom bLeft TDll">出库价</td><td class="bRight bBottom">` + data.ReturnData.res[ i ].OutPrice + `</td><td class="bRight bBottom TDll">周转率</td><td class="bRight bBottom">22.20%</td></tr>`; // 出库价 周转率
+                            alize += `<tr><td class="bRight bBottom bLeft TDll">出库价</td><td class="bRight bBottom">` + data.ReturnData.res[ i ].OutPrice + `</td><td class="bRight bBottom TDll">周转率</td><td class="bRight bBottom">--%</td></tr>`; // 出库价 周转率
                             alize += `<tr><td class="bRight bBottom bLeft TDll">备注</td><td colspan="3" class="bRight bBottom">没有备注</td></tr></table>`; // 备注
                             if( ( i + 1 ) % 2 === 0 && ( i + 1 ) > 1 ) { alize += `<div class="line"></div>`; }
+                            // if( ( i + 1 ) % 5 === 0 && ( i + 1 ) > 4 ) { alize += `<div class="pageTo"></div>`; }
                         }
-                        alize += "</div></div>";
-                        console.log( alize )
-                        window.document.body.innerHTML = alize;
-                        window.print();
+                        alize += `</div><script>let btn = document.querySelector(".print");btn.addEventListener("click", function () {window.print();})</script></div>`;
+                        console.log( alize );
+                        win = window.open();
+                        win.document.write( alize );
                     }
                     else {
                         if( data.ReturnData != null && data.ReturnData.Message ) {
@@ -59,9 +97,9 @@ $.ListView.ActionPreDo = function( actionCode ) {
                         }
                     }
                 }, ( data ) => {
-                     $.IShowError( '操作失败！请联系管理员处理' );
+                    $.IShowError( '操作失败！请联系管理员处理' );
                 }, false );
             }, 1000 )
         }
     }
-};
+}
--- 氚云自定义打印模板 - 友好售后配件标签/H3列表后端.cs
+++ 氚云自定义打印模板 - 友好售后配件标签/H3列表后端.cs
@@ -20,10 +20,6 @@ public class D117502d3f67c8bb71d453c8d8942b37b4eee7f_ListViewController: H3.Smar
         base.OnSubmit(actionName, postValue, response);
         if(actionName == "smallLabel") //打印小标签
         {
-
-        }
-        if(actionName == "BigLabel") // 打印大标签
-        {
             response.ReturnData = new Dictionary<string, object>();
             string OBJs = this.Request["OBJs"] + string.Empty;
             if(string.IsNullOrEmpty(OBJs))
@@ -41,19 +37,52 @@ public class D117502d3f67c8bb71d453c8d8942b37b4eee7f_ListViewController: H3.Smar
                     accountBo = H3.DataModel.BizObject.Load(H3.Organization.User.SystemUserId, this.Request.Engine, "D117502d3f67c8bb71d453c8d8942b37b4eee7f", ObjectIds[i], false);
                     RES.Add(new Dictionary < string, object >
                         {
-                            { "PartNo", string.IsNullOrEmpty(accountBo["PartNoT"].ToString()) ? "--" : accountBo["PartNoT"].ToString() }, // 配件号
-                            { "PartName", string.IsNullOrEmpty(accountBo["Nameofaccessories"].ToString()) ? "--" : accountBo["Nameofaccessories"].ToString() }, // 配件名称
-                            { "Cargospace", string.IsNullOrEmpty(accountBo["CargospaceT"].ToString()) ? "--" : accountBo["CargospaceT"].ToString() }, // 货位号
-                            { "Model", string.IsNullOrEmpty(accountBo["F0000017"].ToString()) ? "--" : accountBo["F0000017"].ToString() }, // 适用车型
-                            { "OutPrice", string.IsNullOrEmpty(accountBo["F0000006"].ToString()) ? "--" : accountBo["F0000006"].ToString() }, // 出库价
-                            // { "remarks", string.IsNullOrEmpty(accountBo["F0000002"].ToString()) ? "--" : accountBo["F0000002"].ToString() }, // 备注
-                            { "BU", string.IsNullOrEmpty(accountBo["BusinessDivisionT"].ToString()) ? "--" : accountBo["BusinessDivisionT"].ToString() }, // 事业部
-                            { "BR", string.IsNullOrEmpty(accountBo["BranchT"].ToString()) ? "--" : accountBo["BranchT"].ToString() }, // 分店
-                        }
-                    );
+                        { "PartNo", string.IsNullOrEmpty(accountBo["PartNoT"].ToString()) ? "--" : accountBo["PartNoT"].ToString() }, // 配件号
+                        { "PartName", string.IsNullOrEmpty(accountBo["Nameofaccessories"].ToString()) ? "--" : accountBo["Nameofaccessories"].ToString() }, // 配件名称
+                        { "Cargospace", string.IsNullOrEmpty(accountBo["CargospaceT"].ToString()) ? "--" : accountBo["CargospaceT"].ToString() }, // 货位号
+                        { "Model", string.IsNullOrEmpty(accountBo["Model"].ToString()) ? "--" : accountBo["Model"].ToString() }, // 适用车型
+                        { "OutPrice", string.IsNullOrEmpty(accountBo["F0000006"].ToString()) ? "--" : accountBo["F0000006"].ToString() }, // 出库价
+                        // { "remarks", string.IsNullOrEmpty(accountBo["F0000002"].ToString()) ? "--" : accountBo["F0000002"].ToString() }, // 备注
+                        // { "BU", string.IsNullOrEmpty(accountBo["BusinessDivisionT"].ToString()) ? "--" : accountBo["BusinessDivisionT"].ToString() }, // 事业部
+                        // { "BR", string.IsNullOrEmpty(accountBo["BranchT"].ToString()) ? "--" : accountBo["BranchT"].ToString() }, // 分店
                 }
-                response.ReturnData.Add("res", RES);
+            );
             }
+            response.ReturnData.Add("res", RES);
         }
     }
+    if(actionName == "BigLabel") // 打印大标签
+    {
+        response.ReturnData = new Dictionary<string, object>();
+        string OBJs = this.Request["OBJs"] + string.Empty;
+        if(string.IsNullOrEmpty(OBJs))
+        {
+            response.ReturnData.Add("Message", "AJAX 未收到任何数据！");
+            return;
+        }
+        else
+        {
+            string[] ObjectIds = OBJs.Split(',');
+            List < Dictionary < string, object >> RES = new List<Dictionary<string, object>>();
+            H3.DataModel.BizObject accountBo = H3.DataModel.BizObject.Load(H3.Organization.User.SystemUserId, this.Request.Engine, "D117502d3f67c8bb71d453c8d8942b37b4eee7f", ObjectIds[0], false);
+            for(int i = 0;i < ObjectIds.Length; i++)
+            {
+                accountBo = H3.DataModel.BizObject.Load(H3.Organization.User.SystemUserId, this.Request.Engine, "D117502d3f67c8bb71d453c8d8942b37b4eee7f", ObjectIds[i], false);
+                RES.Add(new Dictionary < string, object >
+                    {
+                    { "PartNo", string.IsNullOrEmpty(accountBo["PartNoT"].ToString()) ? "--" : accountBo["PartNoT"].ToString() }, // 配件号
+                    { "PartName", string.IsNullOrEmpty(accountBo["Nameofaccessories"].ToString()) ? "--" : accountBo["Nameofaccessories"].ToString() }, // 配件名称
+                    { "Cargospace", string.IsNullOrEmpty(accountBo["CargospaceT"].ToString()) ? "--" : accountBo["CargospaceT"].ToString() }, // 货位号
+                    { "Model", string.IsNullOrEmpty(accountBo["Model"].ToString()) ? "--" : accountBo["Model"].ToString() }, // 适用车型
+                    { "OutPrice", string.IsNullOrEmpty(accountBo["F0000006"].ToString()) ? "--" : accountBo["F0000006"].ToString() }, // 出库价
+                    // { "remarks", string.IsNullOrEmpty(accountBo["F0000002"].ToString()) ? "--" : accountBo["F0000002"].ToString() }, // 备注
+                    { "BU", string.IsNullOrEmpty(accountBo["BusinessDivisionT"].ToString()) ? "--" : accountBo["BusinessDivisionT"].ToString() }, // 事业部
+                    { "BR", string.IsNullOrEmpty(accountBo["BranchT"].ToString()) ? "--" : accountBo["BranchT"].ToString() }, // 分店
+            }
+        );
+        }
+        response.ReturnData.Add("res", RES);
+    }
+}
+}
 }
