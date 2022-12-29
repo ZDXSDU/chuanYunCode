9964d7ba77d5ac172171b88a43014dcd82ae47e1
Z.D.X SDU <2398463125@qq.com>
2022-12-29 20:53:59 +0800

    更新友好售后开单

--- 友好售后开单工时统计.js
+++ 友好售后开单工时统计.js
@@ -7,19 +7,30 @@ $.extend( $.JForm, {
         /** 工时统计 BEGIN **/
         const isAsync = false;
         if( !( $.SmartForm.ResponseContext.IsCreateMode ) ) {
-            this.D148737e8fa914992a741dd9e676959235f4a52.BindChange( '增修工时', () => { AJAX( "IncreaseMaintenanceItems" ); });
-            this.D1487373e21d1c221a44a6c83dba9ba9028ecc8.BindChange( '预估工时', () => { AJAX( "EstimateMaintenanceItems" ); });
+            this.D148737e8fa914992a741dd9e676959235f4a52.BindChange( '增修工时', () => { GetLists(); });
+            this.D1487373e21d1c221a44a6c83dba9ba9028ecc8.BindChange( '预估工时', () => { GetLists(); });
         }
-        function AJAX( sectionName ) {
-            $.SmartForm.PostForm( sectionName, {}, ( data ) => {
-                if( data.Errors && data.Errors.length ) {
-                    $.IShowError( "错误", JSON.stringify( data.Errors ) );
-                } else {
-                    console.log( data.ReturnData );
-                }
-            }, ( error ) => {
-                $.IShowError( "错误", JSON.stringify( error ) );
-            }, isAsync );
+        const GetLists = () => {
+            this.D117502manhour.ClearRows();
+            let AF_detail = this.D1487373e21d1c221a44a6c83dba9ba9028ecc8.GetValue();
+            let CFromControlManager = this.D117502manhour;
+            for( let i = 0;i < AF_detail.length;i++ ) {
+                CFromControlManager.AddRow( $.IGuid(), {
+                    "D117502manhour.maintenanceMeasures": AF_detail[i].maintenanceMeasures, // 维修项目
+                    "D117502manhour.F0000005": AF_detail[i].F0000005, // 修理工
+                    "D117502manhour.chargingMethods": AF_detail[i].chargingMethods // 收费方式
+                })
+            }
+            let BF_detail = this.D148737e8fa914992a741dd9e676959235f4a52.GetValue();            
+            for( let i = 0;i < BF_detail.length;i++ ) {
+                console.log(BF_detail[i].hourlyFees);
+                console.log(BF_detail[i]);
+                CFromControlManager.AddRow( $.IGuid(), {
+                    "D117502manhour.maintenanceMeasures": BF_detail[i].maintenanceMeasures, // 维修项目
+                    "D117502manhour.F0000005": BF_detail[i].F0000005, // 修理工
+                    "D117502manhour.chargingMethods": BF_detail[i].chargingMethods // 收费方式
+                })
+            }
         }
         /** 工时统计 END **/
     },
